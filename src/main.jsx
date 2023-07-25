466ccb05cd93fd7492e3fd937100e54e1aa723af
Muhammad Arifin <muhammadarifin2052@gmail.com>
2023-07-25 22:01:09 +0700

    Rendering Lists

--- src/main.jsx
+++ src/main.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
         element: <RegisterPage />,
     },
     {
-        path: "/product",
+        path: "/products",
         element: <ProductsPage />,
     },
 ]);
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,17 +1,43 @@
 import CardProduct from "../components/fragments/CardProduct";
 
+const products = [
+    {
+        id: 1,
+        name: "Mechanical Keyboard",
+        price: "Rp 700.000",
+        image: "/public/img/mecha-keyboard.jpg",
+        description:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi dolor eveniet, quasi eaque molestiae deserunt velit itaque similique maiores corporis!",
+    },
+    {
+        id: 2,
+        name: "Membrane Keyboard",
+        price: "Rp 300.000",
+        image: "/public/img/mecha-keyboard.jpg",
+        description:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    },
+    {
+        id: 3,
+        name: "Office Keyboard",
+        price: "Rp 200.000",
+        image: "/public/img/mecha-keyboard.jpg",
+        description:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi dolor eveniet, quasi eaque molestiae.",
+    },
+];
 const ProductsPage = () => {
     return (
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header image="/public/img/mecha-keyboard.jpg" />
-                <CardProduct.Body title="Mecha Keyboard">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Excepturi dolor eveniet, quasi eaque molestiae deserunt
-                    velit itaque similique maiores corporis!
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp.700.000" />
-            </CardProduct>
+            {products.map((product) => (
+                <CardProduct key={product.id}>
+                    <CardProduct.Header image={product.image} />
+                    <CardProduct.Body name={product.name}>
+                        {product.description}
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={product.price} />
+                </CardProduct>
+            ))}
         </div>
     );
 };
